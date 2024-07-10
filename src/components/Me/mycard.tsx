277bf87a1bf055dc7dad3f75cb282d99680cbd7d
AlbabRiziq <albabriziq123@gmail.com>
2024-07-10 23:07:09 +0700

    first commit

---
+++ src/components/Me/mycard.tsx
@@ -0,0 +1,20 @@
+import { Link } from "lucide-react"
+
+import {
+    Alert,
+    AlertDescription,
+    AlertTitle,
+} from "@/components/ui/alert"
+
+export function MyCard() {
+    return (
+        <Alert className="max-w-96">
+            <Link className="h-4 w-4" />
+
+            <AlertTitle className="font-bold">JUDUL LINK</AlertTitle>
+            <AlertDescription>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ea hic atque neque nisi aut. Deserunt quibusdam laboriosam odit ducimus tenetur!
+            </AlertDescription>
+        </Alert>
+    )
+}
